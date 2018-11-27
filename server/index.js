0dcc31a81552d69eeaaa21aa0475a5bbc19d9c59
Anders Åkerö <anders.akero@motionpicturesolutions.com>
2018-11-27 21:44:45 +0000

    Adding express and creating start script

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+// Listen to port that is sent by environment variable called "PORT"
+// If env parameter "PORT" is not set, then use port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
